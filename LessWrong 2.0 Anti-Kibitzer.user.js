d992984e471e831a7649a3775da3e54ef9372b38
Evan R. Murphy <evan@evanrmurphy.com>
2022-07-05 23:46:24 -0700

    Fix vote score hiding, also hide user profile stats

--- LessWrong 2.0 Anti-Kibitzer.user.js
+++ LessWrong 2.0 Anti-Kibitzer.user.js
@@ -56,7 +56,15 @@ function censorship_sweep() {
     update_censor_text(".UsersNameDisplay-userName", colorful_censor_bar, false);
     update_censor_text(".NotificationsItem-notificationLabel", notification_censor_bar, false);
     update_censor_text(".PostsVote-voteScore", vote_censor_bar, true);
-    update_censor_text(".CommentsVote-voteScore", vote_censor_bar, true);
+
+    // Evan: Fixing vote score and user stats hiding
+    // 
+    // update_censor_text(".CommentsVote-voteScore", vote_censor_bar, true);
+    update_censor_text(".OverallVoteAxis-voteScore", vote_censor_bar, true);
+    update_censor_text(".AgreementVoteAxis-agreementScore", vote_censor_bar, true);
+    update_censor_text(".SingleLineComment-karma", vote_censor_bar, true);
+    update_censor_text(".UsersProfile-meta", vote_censor_bar, true);
+
     update_censor_text(".PostsItem2-karma", vote_censor_bar, true);
 }
 
